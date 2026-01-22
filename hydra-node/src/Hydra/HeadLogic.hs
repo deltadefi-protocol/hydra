0142966396f5ec9b2ff91ed733c116654e3fc33d
hinsonsidan <wongkahinhinson@gmail.com>
2026-01-22 18:19:53 +0800

    ci: format

--- hydra-node/src/Hydra/HeadLogic.hs
+++ hydra-node/src/Hydra/HeadLogic.hs
@@ -1725,19 +1725,21 @@ aggregate st = \case
                       , currentDepositTxId = Nothing
                       }
                   ConfirmedSnapshot{snapshot = Snapshot{utxo, utxoToCommit, version = snapshotVersion}} ->
-                    let -- If current version > snapshot version, commit was finalized on L1
-                        -- Include utxoToCommit in localUTxO since the UTxO is now in the head
-                        committedUTxO =
-                          if version > snapshotVersion
-                            then fromMaybe mempty utxoToCommit
-                            else mempty
-                     in coordinatedHeadState
-                          { localUTxO = utxo <> committedUTxO
-                          , localTxs = mempty
-                          , allTxs = mempty
-                          , seenSnapshot = LastSeenSnapshot snapshotNumber
-                          , currentDepositTxId = Nothing
-                          }
+                    let
+                      -- If current version > snapshot version, commit was finalized on L1
+                      -- Include utxoToCommit in localUTxO since the UTxO is now in the head
+                      committedUTxO =
+                        if version > snapshotVersion
+                          then fromMaybe mempty utxoToCommit
+                          else mempty
+                     in
+                      coordinatedHeadState
+                        { localUTxO = utxo <> committedUTxO
+                        , localTxs = mempty
+                        , allTxs = mempty
+                        , seenSnapshot = LastSeenSnapshot snapshotNumber
+                        , currentDepositTxId = Nothing
+                        }
             }
       _otherState -> st
   DepositRecorded{} -> st
